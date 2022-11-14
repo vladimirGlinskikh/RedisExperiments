de78878c60d4aaf647da154a10f80d81e53ef66d
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2022-11-14 04:11:26 +0300

    Start project

---
+++ src/main/java/kz/zhelezyaka/redisexperiment/RedisExperimentApplication.java
@@ -0,0 +1,12 @@
+package kz.zhelezyaka.redisexperiment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisExperimentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisExperimentApplication.class, args);
+    }
+}
---
+++ src/test/java/kz/zhelezyaka/redisexperiment/RedisExperimentApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.zhelezyaka.redisexperiment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisExperimentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
